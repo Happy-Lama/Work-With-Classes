5f393bf6200d7144c950b84521b8193438df25a7
Happy-Lama <kldenis2001@gmail.com>
2020-03-15 19:15:03 +0300

    new changes now including student id

--- Student creator.py
+++ Student creator.py
@@ -1,24 +1,27 @@
+from identification import id
 class student:
     number_of_students = 0
-    def __init__(self,name,age,subjects):
+    def __init__(self,name,age,subjects,student_class):
         self.name = name
         self.age = age
         self.subjects = subjects
+        self.std_id = id(student_class)
         student.number_of_students += 1
     @classmethod
-    def create_student_profile(cls,student_info):
-        name, age ,subjects = student_info.split('-')
+    def create_student_profile(cls,students_info):
+        name, age, subjects, student_class = students_info.split('-')
         age = int(age)
+        student_class = int(student_class)
         subjects = subjects.split(',')
         subjects[2] = subjects[2].replace('\n','')
-        return cls(name, age, subjects)
-std_number = {}
+        return cls(name, age, subjects, student_class)
+_stdts_ = {}
 keys = []
-index = 1
-students = open('Students.txt','r')
-for line in students:
-    students_info = line
-    std_number['Std_{}'.format(index)] = student.create_student_profile(students_info)
-    keys.append('Std_{}'.format(index))
-    index += 1
-students.close()
+with open('Students.txt','r') as students:
+    for index,line in enumerate(students,1):
+        students_info = line
+        stdt = student.create_student_profile(students_info)
+        _stdts_[stdt.std_id] = stdt
+        keys.append(stdt.std_id)
+for key in keys:
+    print( key,', ',_stdts_[key])
---
+++ identification.py
@@ -0,0 +1,13 @@
+from random import randint
+def id(student_class):
+    alphabet = ['A','B','C','D','E','F']
+    id_no = ''
+    while True:
+        for index,char in enumerate(alphabet,1):
+            if student_class == index:
+                id_no = id_no + char
+        for i in range(9):
+            num = randint(0,9)
+            id_no = id_no + str(num)
+        
+        return id_no
